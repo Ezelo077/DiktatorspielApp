52e1d8c81f45b7957a7a9452201e62aeaefee184
Ezelo077 <erick.franz@gmx.net>
2025-11-16 15:45:50 +0100

    Update service worker to version 1.0.4

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = 'v1.0.3';
+const CACHE_VERSION = 'v1.0.4';
 const CACHE_NAME = `diktatorspiel-${CACHE_VERSION}`;
 
 const ASSETS = [
@@ -41,4 +41,5 @@ self.addEventListener('fetch', (event) => {
     })
   );
 });
+
 
