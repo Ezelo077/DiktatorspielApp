4f1620cb2050a53f542c73dc1366cff9f2c5771c
Ezelo077 <erick.franz@gmx.net>
2025-11-16 16:09:04 +0100

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = 'v1.0.6';
+const CACHE_VERSION = 'v1.0.7';
 const CACHE_NAME = `diktatorspiel-${CACHE_VERSION}`;
 
 const ASSETS = [
@@ -41,6 +41,7 @@ self.addEventListener('fetch', (event) => {
     })
   );
 });
+
 
 
 
